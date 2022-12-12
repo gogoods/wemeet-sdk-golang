cbf3f0fe0c86e95f499f4fae0d6e4a49b282e459
willlv <willlv@tencent.com>
2022-12-12 11:25:47 +0800

    init

--- examples/main.go
+++ examples/main.go
@@ -5,10 +5,10 @@ import "github.com/gogoods/wemeet-sdk-golang/wemeet"
 var meeting = wemeet.Meeting{
 	Registered: wemeet.EnableRegistered,
 	Version:    "1.0.0",
-	AppID:      "219284983",
-	SdkID:      "20980278515",
-	SecretID:   "DPepEnCDqdXiUnAG9Gd5CEnrOGUe8EQR",
-	SecretKey:  "qKJBsi5Nl0oUOy0vF4Gtye91XaezWfFCTM7e6g0qNI1f3Gut",
+	AppID:      "xxx",
+	SdkID:      "xxx",
+	SecretID:   "xxx",
+	SecretKey:  "xxx",
 }
 
 func main() {
